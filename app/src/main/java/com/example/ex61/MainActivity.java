666786a4c294e6217d1de42740cbca2f30048831
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-01-07 22:15:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex61/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.ex61;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    ImageView iv;
+
+    int rndNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        iv = findViewById(R.id.iv);
+
+
+    }
+
+
+    public void clicked(View view) {
+        Random rnd = new Random();
+        rndNumber= rnd.nextInt(3)+1;
+        btn.setText(String.valueOf(rndNumber));
+        if (rndNumber==1){
+            iv.setImageResource(R.drawable.gossip_girl);
+        }
+        if (rndNumber==2){
+            iv.setImageResource(R.drawable.lucifer);
+        }
+        if (rndNumber==3){
+            iv.setImageResource(R.drawable.galis);
+        }
+    }
+}
